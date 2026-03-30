75666cdf04741f8d495839cb971e9f6cbb8be9e3
KnightsAlexandria-FS <aknights@student.fullsail.edu>
2026-03-29 23:31:48 -0400

    update string
    Removed unused functions that I was unable to get to work properly from the test imports and updated for sumofArray and isPalindrome

--- Knights_Alexandria/index.test.js
+++ Knights_Alexandria/index.test.js
@@ -1,8 +1,12 @@
-const { sumOfArray, reverseString, findMax, isPalindrome } = require("./main");
+const { sumOfArray, isPalindrome } = require("./main");
 
 describe("Assignment Function Tests", () => {
   test("sumOfArray logic", () => {
     expect(sumOfArray([1, 2, 3])).toBe(6);
     expect(sumOfArray("not an array")).toBe(0);
+  });
+  test("isPalindrome logic", () => {
+    expect(isPalindrome("racecar")).toBe(true);
+    expect(isPalindrome("hello")).toBe(false);
   });
 });
